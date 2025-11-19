873a7bf42a754a507932d44ee4986e2d1bc18f36
Dario Coletto <darocoletto@gmail.com>
2025-11-19 10:54:03 -0300

    app express init code

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+
+import express from 'express';
+
+const app = express();
+
+
+
+
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+app.listen(port, ()=> console.log(`API on http://localhost:${port}`));
+
+export default app;
